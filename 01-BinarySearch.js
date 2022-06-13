2707fe18744cb4e17b3b3c275e671ecf2f635920
Tomas Garza <99302916+tdgarza@users.noreply.github.com>
2022-06-12 20:52:40 -0500

    Segundo intento en que quede perfecto, solo agrupe el primer If

--- 01-BinarySearch.js
+++ 01-BinarySearch.js
@@ -1,31 +1,29 @@
+let arr = [1, 3, 5, 7, 9,11,13,15,17,19,21];
+console.log(arr);
+let low = 0;
+let high = 10;
+let item = 6;
 
-        //function binary_search(arr, item){
-          let arr=[2,4,6,8,10,12,14,16,18,20];
-          console.log(arr);
-            let low =4;
-            let high=9;
-           // let high=arr[i]-1;
-           let item=6;
-            console.log(arr[high]);
-            console.log(arr[low]);
-          while(low<=high){
-                let mid=Math.floor((low+high)/2);
-                console.log("Valor del primer mid: "+mid);
-                console.log("Valor del arreglo: "+ arr[mid]);
-                let guess=arr[mid];
-                console.log("valor del Guess: "+guess);
-            
-            if (guess == item)
-           arr[mid];
-           console.log("primer if: " +arr[mid]);
-            if (guess >item){
-            high=mid-1;
-            console.log("Valor del segundo IF: "+high);
-        }
-            else{
-            low=mid+1;
-            console.log("Valor del Else: "+low);
-            }
-           }
-   
-      
+console.log("Valor del Array con el valor mas alto: " + arr[high]);
+console.log("Valor del Array con el valor mas bajo: " + arr[low]);
+
+while (low <= high) {
+  let mid = Math.floor((low + high) / 2);
+  console.log("Valor del primer mid: " + mid);
+  console.log("Valor del arreglo: arr[mid] " + arr[mid]);
+  let guess = arr[mid];
+  console.log("valor del Guess: " + guess);
+
+  if (guess == item) {
+    arr[mid];
+    console.log("primer if: arr[mid] " + arr[mid]);
+  
+  if (guess > item) {
+    high = mid - 1;
+    console.log("Valor del segundo IF: " + high);
+  } }else {
+    low = mid + 1;
+    console.log("Valor del Else: " + low);
+  }
+  console.log("Valor del arreglo: arr[mid] " + arr[mid]);
+}
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 Vamos por el primero del libro
 
-12 de Junio del 22.
+10 de Junio del 22.
+
+12 de Junio del 22, vamos a empezar a leer el siguiente ejercicio del libro y comprenderlo.
