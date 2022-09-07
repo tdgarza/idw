01f39d3dc41cfcf35aff8cb8eae8c5ae99062a23
tdgarza <you@example.com>
2022-09-07 17:31:50 -0500

    No se pudo aun, se hace infinfinito el programa

--- 13-MetodosNumericos-NewtonRaphson.js
+++ 13-MetodosNumericos-NewtonRaphson.js
@@ -3,22 +3,19 @@ function myFunction(){
     let xrold=0;
     console.log("El valor inicial x: " +x);
     do{
+
     let e0=Math.exp(-x)-x;
     let e1=-(Math.exp(-x))-1;
-   
-    console.log("El valor de la xoriginal: " +e0);
-    console.log("El valor de la xderivada: " +e1);
-
-    let xi=x-(e0/e1);
-    console.log("El valor de siguiente: " +xi);
+    x=x-(e0/e1);
+    console.log("El valor de siguiente xi: " +x);
     
-    console.log("El valor del Xrold: " +xrold);
-    var error=Math.abs((xi-xrold)/xi)*100;
+    var error=Math.abs((x-xrold)/x)*100;
     console.log("El valor del error: " +error);
     xrold=x;
+    
     }
     while(error>1);
     let text=xrold;
-    console.log("Fin con el Text " +text);
+    
     document.getElementById("sumando").innerHTML = text;  
     }
